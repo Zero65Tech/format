cd0cd073bb2221352bb378ae4a064adc3264eed3
Prashant Gupta <antshpra@gmail.com>
2023-12-18 23:51:32 +0530

    number and currency

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-let prettyJson = (data, breakFn, ...nodes) => {
+exports.number = (number, d = 0) => {
+
+  if(!number || Math.abs(number) < 0.0001)
+    return null;
+
+  number = Math.round(number * Math.pow(10, d));
+
+  let numberStr = Math.abs(number) + '';
+  let l = numberStr.length;
+  for(; l < d; l++)
+    numberStr = '0' + numberStr;
+
+  if(l == d)
+    numberStr = '0.' + numberStr;
+  else if(l <= 3 + d)
+    numberStr = numberStr.substring(0, l - d) + (d ? '.' + numberStr.substring(l - d) : '');
+  else if(l <= 5 + d)
+    numberStr = numberStr.substring(0, l - 3 - d) + ',' + numberStr.substring(l - 3 - d, l - d) + (d ? '.' + numberStr.substring(l - d) : '')
+  else
+    numberStr = numberStr.substring(0, l - 5 - d) + ',' + numberStr.substring(l - 5 - d, l - 3 - d) + ',' + numberStr.substring(l - 3 - d, l - d) + (d ? '.' + numberStr.substring(l - d) : '');
+  
+  return (number < 0 ? '- ' : '') + numberStr;
+
+};
+
+exports.currency = (amount, d = 2) => {
+  let f = exports.number(Math.abs(amount), d);
+  if(f === null)
+    return null;
+  return amount < 0 ? ('- ₹' + f) : ('₹' + f);
+}
+
+exports.json = (data, breakFn, ...nodes) => {
   
   if(typeof data != 'object') {
 
@@ -17,7 +49,7 @@ let prettyJson = (data, breakFn, ...nodes) => {
 
     for(let [ idx, item ] of data.entries()) {
       if(typeof item == 'object' && item !== null) {
-        strArr.push(prettyJson(item, breakFn, ...nodes, idx).replace(/\n/g, '\n  '));
+        strArr.push(exports.json(item, breakFn, ...nodes, idx).replace(/\n/g, '\n  '));
       } else if(typeof item == 'number' && Number.isNaN(item)) {
         strArr.push('"NaN');
       } else {
@@ -41,7 +73,7 @@ let prettyJson = (data, breakFn, ...nodes) => {
     let strArr = [];
     for(let [ key, value ] of Object.entries(data)) {
       if(typeof value == 'object' && value !== null)
-        strArr.push(`"${ key }": ${ prettyJson(value, breakFn, ...nodes, key).replace(/\n/g, '\n  ') }`);
+        strArr.push(`"${ key }": ${ exports.json(value, breakFn, ...nodes, key).replace(/\n/g, '\n  ') }`);
       else if(typeof value == 'number' && Number.isNaN(value))
         strArr.push(`"${ key }": "NaN"`);
       else
